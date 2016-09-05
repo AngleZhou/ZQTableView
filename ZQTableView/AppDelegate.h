a04fa93e053efcd9abcfcaa0bc1e540f47ca780b
AngleZhou <zhouq87724@163.com>
2016-09-05 16:05:51 +0800

    initial

---
+++ ZQTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZQTableView
+//
+//  Created by Zhou Qian on 16/9/5.
+//  Copyright © 2016年 Zhou Qian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
